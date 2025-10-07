51c4dd0092a9c25da276c355e1e410c7d51c5a40
Bruce Li <isanoob1897@outlook.com>
2025-10-07 04:28:12 +0000

    start project

---
+++ src/main/java/dev/desktop/AutoScript/AutoScript.java
@@ -0,0 +1,7 @@
+package dev.desktop.AutoScript;
+
+public class AutoScript {
+    public static void license() {
+        System.out.println("AutoScript is licensed under a Modified BSD License.\nNO WARRANTY OF ANY KIND!");
+    }
+}
---
+++ src/main/java/dev/desktop/AutoScript/IO.java
@@ -0,0 +1,9 @@
+package dev.desktop.AutoScript;
+
+public class IO {
+    public void println(String input) {
+        System.out.println(input);
+    }
+    // todo, overload method
+    // todo, add docs
+}
